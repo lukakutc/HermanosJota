36430a45df13da7f85db3b86e18add2c4a81d6cf
jorge.ugarte <jorgeluisugartecuevas@gmail.com>
2025-09-06 23:33:43 -0300

    Se trabajo en el CSS de la pagina principal

---
+++ JorgeUgarte/js/index.js
@@ -0,0 +1,18 @@
+//PRODUCTOS DESTACADOS
+const listaDestacados = document.querySelector("#lista-destacados");
+
+const destacados = productos.filter(p => p.destacado);
+
+destacados.forEach(producto => {
+  const item = document.createElement("li");
+  item.classList.add("producto-destacado");
+  item.innerHTML = `
+    <a href="producto.html?id=${producto.id}">
+      <img src="${producto.imagen}" alt="${producto.nombre}">
+      <h3>${producto.nombre}</h3>
+      <p class="precio">$${producto.precio.toLocaleString("es-AR")}</p>
+    </a>
+  `;
+  listaDestacados.appendChild(item);
+});
+
