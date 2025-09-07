7bb5097e346af13c89bef308dc64f9754eb9f98c
agus10c <acaceresrapetti@gmail.com>
2025-09-07 18:27:32 -0300

    mejora de estilos en productos.html

--- AgustinCaceres/js/productos.js
+++ AgustinCaceres/js/productos.js
@@ -6,7 +6,7 @@ productos.forEach(producto => {
   item.classList.add("tarjeta-producto");
   item.innerHTML = `
     <a href="producto.html?id=${producto.id}">
-      <img src="${producto.imagen}" alt="${producto.nombre}">
+      <img src="${producto.imagen}" alt="${producto.nombre}" class="imagen-producto">
       <h3>${producto.nombre}</h3>
       <p class="precio">$${producto.precio.toLocaleString("es-AR")}</p>
     </a>
