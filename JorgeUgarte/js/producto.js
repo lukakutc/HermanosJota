8f64a99c2160a789e800d2179aac24a4fc3c4171
jorge.ugarte <jorgeluisugartecuevas@gmail.com>
2025-09-07 21:14:40 -0300

    Se trabajo en el CSS de la pagina Producto eh Index

--- JorgeUgarte/js/producto.js
+++ JorgeUgarte/js/producto.js
@@ -1,5 +1,6 @@
-
-const detalleProducto = document.querySelector("#detalle-producto");
+const detalleProducto = document.querySelector(".detalle-producto");
+const imagenProducto = document.querySelector(".imagen-producto");
+const tbody = document.getElementById("tabla-producto");
 
 
 // Obtener el parámetro id de la URL
@@ -9,14 +10,32 @@ const id = parseInt(params.get("id"), 10);
 // Buscar el producto correspondiente
 const producto = productos.find(p => p.id === id);
 
-if(producto){
-  const item = document.createElement("div");
-  item.innerHTML= `
-    <h1> Producto: ${producto.nombre} </h1>
-      <img src="${producto.imagen}" alt="${producto.nombre}">
-      <h3>${producto.nombre}</h3>
-      <p class="precio">$${producto.precio.toLocaleString("es-AR")}</p>
-  `;
-  detalleProducto.appendChild(item);
+if (producto) {
+    // Imagen
+    const img = document.createElement("img");
+    img.src = producto.imagen;
+    img.alt = producto.nombre;
+    imagenProducto.appendChild(img);
+
+    const item = document.createElement("div");
+    item.innerHTML = `
+        <h1>${producto.nombre}</h1>
+        <p class="descripcion">${producto.descripcion}</p>
+        <p class="precio">$${producto.precio.toLocaleString("es-AR")}</p>
+    `
+
+    detalleProducto.insertBefore(item, detalleProducto.firstChild);
+
+    for (const [titulo, valor] of Object.entries(producto.detalles)) {
+        const fila = document.createElement("tr");
+
+        fila.innerHTML = `
+        <th class="titulo">${titulo}</th>
+        <td class="valor">${valor}</td>
+    `;
+
+        tbody.appendChild(fila);
+    }
 }
+
   
